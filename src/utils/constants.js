f8dc73787bbac2006a431f221678bf25fb1638fe
Chandan <hb7102849@gmail.com>
2023-12-01 00:23:43 +0530

    initialize

---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const Netflix_Bg="https://assets.nflxext.com/ffe/siteui/vlv3/c906271d-7184-4eec-83c9-b6d4c1a068ec/728874a6-eeda-400a-9bcf-a935a1408a4f/IN-en-20231127-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
