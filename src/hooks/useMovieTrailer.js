d3850dbafe2d83a62f9c8c9a3b463ec11608aee1
Chandan <hb7102849@gmail.com>
2023-12-15 17:01:36 +0530

    Optimization

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,12 +1,12 @@
 
 import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addTrailerVideo } from '../utils/movieSlice';
 import { useEffect } from 'react';
 const useMovieTrailer = (movieId) => {
 
     const dispatch=useDispatch();
-    
+    const trailerVideo=useSelector(store=>store.movies.trailerVideo);
     const getMovieVideos=async()=>{
         const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US', API_OPTIONS);
         const json=await data.json();
@@ -19,7 +19,9 @@ const useMovieTrailer = (movieId) => {
         console.log(trailer);
     }
     useEffect(()=>{
+       
         getMovieVideos();
+     
     },[ ])
 
 }
