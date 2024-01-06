8dd3a9ce55dfdeda7286dc10afeaca05e194ac62
Chandan <hb7102849@gmail.com>
2024-01-07 02:07:07 +0530

    bug fixed

--- src/components/SecoundaryContainer.js
+++ src/components/SecoundaryContainer.js
@@ -10,7 +10,7 @@ const SecoundaryContainer = () => {
     <div >
       {movies.nowPlayingMovies && (
         <div className='bg-black' >
-          <div className="mt-0 md:-mt-40 pl-4 md:pl-12 relative z-20 mobile-view-sec">
+          <div className="mt-0 md:-mt-48 pl-4 md:pl-12 relative z-20 mobile-view-sec">
           <Movielist title={"Now Playing"} movies={movies?.nowPlayingMovies} />
         <Movielist title={"Top Rated"} movies={movies?.topRatedMovies} />
        
